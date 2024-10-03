91bde26b610f3b4aea19c5bc62193411280c93ff
George <mr.georgemanea@gmail.com>
2024-10-03 16:14:48 +0200

    done

---
+++ app/src/main/java/com/example/practica2_1_george_manuel/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.practica2_1_george_manuel
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }*/
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+
+        val jugador = findViewById(R.id.button10) as Button
+        jugador.setOnClickListener { lanzarNewPlayer()}
+    }
+
+    fun lanzarNewPlayer(){
+        val i = Intent(this, NewPlayer::class.java)
+        startActivity(i)
+    }
+}
